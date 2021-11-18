081a6f30cadf1d0ea0df804a07f01d875d7d7d5c
Peszko-Patryk <patrickpeszko@gmail.com>
2021-11-18 13:00:57 +0100

    added median, maximum, minimum, arithmetic, geometric test

--- testowanie2/src/test/java/meansTest.java
+++ testowanie2/src/test/java/meansTest.java
@@ -1,69 +1,78 @@
+package java;
+
 import static org.junit.Assert.*;
 
 public class meansTest {
+    private Means mean = new Means();
+    private double[] numbers = {108, 3, 18, 2, 9};
 
     @org.junit.Test
-    public void median() {
+    public void medianTest() {
+        double median = mean.getMedian(numbers);
+
+        assertEquals(9, median);
+    }
+
+    @org.junit.Test
+    public void maximumTest() {
+        double maximum = mean.getMaximum(numbers);
+
+        assertEquals(108, maximum);
+    }
+
+    @org.junit.Test
+    public void minimumTest() {
+        double minimum = mean.getMinimum(numbers);
+
+        assertEquals(2, minimum);
+    }
+
+    @org.junit.Test
+    public void arithmeticMeanTest() {
+        double arithmeticMean = mean.getArithmeticMean(numbers);
+
+        assertEquals(28, arithmeticMean);
+    }
+
+    @org.junit.Test
+    public void geometricMeanTest() {
+        double geometricMean = mean.getGeometricMean(numbers);
+
+        assertEquals(18, geometricMean);
+    }
+
+    @org.junit.Test
+    public void harmonicMeanTest() {
         fail();
         //TODO
     }
 
     @org.junit.Test
-    public void maximum() {
+    public void powerMeanTest() {
         fail();
         //TODO
     }
 
     @org.junit.Test
-    public void minimum() {
+    public void quadraticMeanTest() {
         fail();
         //TODO
     }
 
     @org.junit.Test
-    public void arithmeticMean() {
+    public void logarithmicMeanTest() {
         fail();
         //TODO
     }
 
     @org.junit.Test
-    public void geometricMean() {
+    public void truncatedMeanTest() {
         fail();
         //TODO
     }
 
     @org.junit.Test
-    public void harmonicMean() {
-        fail();
-        //TODO
-    }
-
-    @org.junit.Test
-    public void powerMean() {
-        fail();
-        //TODO
-    }
-
-    @org.junit.Test
-    public void quadraticMean() {
-        fail();
-        //TODO
-    }
-
-    @org.junit.Test
-    public void logarithmicMean() {
-        fail();
-        //TODO
-    }
-
-    @org.junit.Test
-    public void truncatedMean() {
-        fail();
-        //TODO
-    }
-
-    @org.junit.Test
-    public void winsorizedMean() {
+    public void winsorizedMeanTest() {
         fail();
         //TODO
     }
